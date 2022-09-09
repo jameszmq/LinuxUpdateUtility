d9efa921f796118c94194cd9a0ff19f15fdf38f0
Mingqi Zhang <jameszmq@gmail.com>
2022-09-08 23:04:51 -0700

    Added the main script and script to create alias.

---
+++ add_alias.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+path=$(pwd)
+filepath="${path}/upgrade_utility.py"
+
+if [ "$#" -ne 1 ]; then
+	printf "Illegal number of parameters, should be 1.\n"
+	exit -1
+fi
+
+if ! alias $1 2>/dev/null ; then
+	printf "\nalias $1=\"${filepath}\"" >> ~/.bash_aliases
+else
+	printf "Alias \"$1\" already exists. Please try using another alias.\n"
+fi
---
+++ upgrade_utility.py
@@ -0,0 +1,81 @@
+import os
+
+def apt_update():
+	prompt = "Updating apt"
+	print(prompt.center(os.get_terminal_size().columns, '-'))
+	os.system("sudo apt-get update")
+	os.system("sudo apt-get full-upgrade")
+	os.system("sudo apt-get autoremove")
+	os.system("sudo apt-get autoclean")
+
+def yum_update():
+	prompt = "Updating yum"
+	print(prompt.center(os.get_terminal_size().columns, '-'))
+	os.system("sudo yum update")
+
+def pacman_update():
+	prompt = "Updating pacman"
+	print(prompt.center(os.get_terminal_size().columns, '-'))
+	os.system("sudo pacman -Syu")
+
+def pip_update():
+	prompt = "Updating pip"
+	print(prompt.center(os.get_terminal_size().columns, '-'))
+	os.system("python3 -m pip list --outdated > a.txt")
+	fp = open("a.txt", "r")
+	lines = fp.readlines()
+	if len(lines) > 2:
+		del lines[0]
+		del lines[0]
+		result = ""
+		for line in lines:
+			if line != "":
+				result += line.split()[0]
+				result += " "
+		os.system("python3 -m pip install --upgrade " + result)
+	else:
+		print("Nothing to update.")
+	fp.close()
+	os.system("python3 -m pip cache purge")
+	os.remove("a.txt")
+
+def gem_update():
+	prompt = "Updating gem"
+	print(prompt.center(os.get_terminal_size().columns, '-'))
+	os.system("sudo gem update")
+
+def exit_update():
+	print("Thanks for using. Bye!")
+	exit(0)
+
+def prompt():
+	print("Please select the upgrade you want to run:")
+	for option in option_list:
+		print(option[0] + ". " + option[1])
+	selection = input('Selection: ').lower()
+	for char in selection:
+		for option in option_list:
+			if char == option[0]:
+				option[2] = True
+	for option in option_list:
+		if option[2]:
+			option[3]()
+			option[2] = False
+
+option_list = [
+	["1", "pip", 		False, pip_update],
+	["2", "gem", 		False, gem_update],
+	["3", "apt", 		False, apt_update],
+	["4", "yum", 		False, yum_update],
+	["5", "pacman", 	False, pacman_update],
+	["6", "exit", 		False, exit_update]
+]
+
+try:
+	print("Welcome to the Linux Upgrade Utility!")
+	while True:
+		prompt()
+	exit(0)
+except KeyboardInterrupt:
+	print("\nCtrl + C received. Exiting.")
+	exit(0)
